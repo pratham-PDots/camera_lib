6b50d368f7f82bd231852ef94b14b925e01517a3
Saurabh <saurabhjaiswal932@gmail.com>
2023-08-18 12:11:46 +0530

    changes in args

--- camera-lib-android/src/main/java/com/sj/camera_lib_android/Demo.kt
+++ camera-lib-android/src/main/java/com/sj/camera_lib_android/Demo.kt
@@ -3,7 +3,7 @@ package com.sj.camera_lib_android
 import android.content.Context
 import android.widget.Toast
 
-public class Demo {
+object Demo {
     fun showMessage(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
